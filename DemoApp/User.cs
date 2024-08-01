cb101c7ecd882a73d6b666b954b20459e837d402
Nozimjon04 <usminaliyevnozimjon@gmail.com>
2024-08-01 22:25:19 +0500

    UserName prop added

--- DemoApp/User.cs
+++ DemoApp/User.cs
@@ -5,4 +5,6 @@ public class User
     public int MyProperty { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
+    public string Password { get; set; }
+    public string UserName { get; set; }
 }
