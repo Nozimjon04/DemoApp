529d405a35f7951c48951f488c597164161c4ca2
Nozimjon04 <usminaliyevnozimjon@gmail.com>
2024-08-01 22:08:36 +0500

    user class updated

--- DemoApp/Product.cs
+++ DemoApp/Product.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DemoApp;
 
-namespace DemoApp
+public class Product
 {
-    internal class Product
-    {
-    }
 }
